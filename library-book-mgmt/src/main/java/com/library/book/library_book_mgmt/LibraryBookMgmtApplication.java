a918664f790330170cb369b501b88d51594b9850
MohsinAzeem <mohsinazeemrind@gmail.com>
2025-04-20 01:12:25 +0500

    initial commit

---
+++ library-book-mgmt/src/main/java/com/library/book/library_book_mgmt/LibraryBookMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.library.book.library_book_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookMgmtApplication.class, args);
+	}
+
+}
---
+++ library-book-mgmt/src/test/java/com/library/book/library_book_mgmt/LibraryBookMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.book.library_book_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBookMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
